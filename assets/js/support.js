86d943b7761f0c16cc0e64a85138e8ab7beae9d6
it-id-xyz <ayamsayrrr78@gmail.com>
2026-01-31 21:50:57 +0700

    Update support.js

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,4 +1,4 @@
-import { showAdminUI } from "assets/js/role.js";
+import { showAdminUI } from "./role.js";
 
 // === 1. CEK ROLE DULU ===
 const role = localStorage.getItem("role");
@@ -23,6 +23,7 @@ if (btnUpdate) {
     // logic update nanti di sini
   });
 }
+
 
 
 
