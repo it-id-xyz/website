80d4b119d04218d160150efea4f3c07e43eb8d62
it-id-xyz <ayamsayrrr78@gmail.com>
2026-02-03 09:15:02 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,20 @@ const navMenu = document.getElementById('navMenu');
 const back = document.getElementById('back');
 const alurIt = document.getElementById('alurIt');
 
-
+requireAdmin()
+  .then(() => {
+    // KODE DI BAWAH HANYA JALAN JIKA USER ADALAH ADMIN
+    console.log("Admin terdeteksi, mengaktifkan fitur admin...");
+    const cEl = document.createElement('a');
+    cEl.id = 'admin-only';
+    cEl.innerText ='<i class="fa fa-gauge-high"></i> Admin Dashboard';
+    cEl.href = 'admin.html';
+    initAdminFeatures(); 
+  })
+  .catch(() => {
+    // Jika bukan admin/guest, biarkan saja, jangan di-redirect
+    console.log("Bukan admin, fitur admin dinonaktifkan.");
+  });
 
 menuToggle.addEventListener('click', () => {
   navMenu.classList.remove('hidden');
@@ -11,4 +24,5 @@ menuToggle.addEventListener('click', () => {
 
 back.addEventListener('click', () => {
     navMenu.classList.add('hidden');
+
 });
