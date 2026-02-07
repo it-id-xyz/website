de514d34a528b72af3bce6234b8324029ffd132f
it-id-xyz <ayamsayrrr78@gmail.com>
2026-02-07 08:32:23 +0700

    Add files via upload

---
+++ assets/js/lita.js
@@ -0,0 +1,63 @@
+
+const btnSubmit = document.getElementById('send-btn');
+const now = new Date();
+const jam = now.getHours().toString().padStart(2, '0') + now.getMinutes().toString().padStart(2, '0');
+
+async function sendQuest() {
+    const inputText = document.getElementById('isi-text');
+    const isiText = inputText.value;
+
+    if(isiText.trim() === '') {
+        alert('error, harap isi teks');
+        return;
+    }
+
+    const chatBox = document.getElementById('chat-box');
+    const cardChat = createElement('div');
+    chatBox.innerHTML =
+    cardChat.classList = 'message incoming';
+    cardChat.innerHTML = `
+    <div class="bubble">Waiting for response..
+    <span class="time">${jam}</span>
+    </div>
+    `;
+
+    try {
+        const response = await fetch('api/chat.js',{
+            method: 'POST',
+            header: {
+                'content-type': 'aplication/json'
+            },
+            body: JSON.stringify({question: userQuestion})
+        });
+        const data = await response.json();
+        if(data.error) {
+            chatBox.innerHTML = `
+            <div class="message incoming">
+                <div class="bubble">${data.error}
+                <span class="time">${jam}</span>
+                </div>
+            </div>`
+        } else {
+            chatBox.innerHTML = `
+            <div class="message incoming">
+                <div class="bubble">${data.answer}
+                <span class="time">${jam}</span>
+                </div>
+            </div>
+            `;
+        }
+    } catch(error) {
+        chatBox.innerHTML = `
+        <div class="message incoming">
+            <div class="bubble">Error jaringan, coba lagi ya
+            <span class="time">${jam}</span>
+            </div>
+        </div>
+        `;
+    }
+    inputText.value = '';
+} 
+
+btnSubmit.addEventListener('click', sendQuest);
+    
