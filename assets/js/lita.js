2102310a705dd5303929436f77ba6091546875bc
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-02-08 03:17:52 +0700

    Update lita.js

--- assets/js/lita.js
+++ assets/js/lita.js
@@ -1,7 +1,21 @@
-
 const btnSubmit = document.getElementById('send-btn');
 const now = new Date();
 
+function btnCopy(block) {
+    const button = document.createElement('button');
+    button.innerText = 'Copy';
+    button.className = 'copy-btn';
+    
+    // Taruh tombol di atas blok kode
+    block.parentNode.style.position = 'relative';
+    block.parentNode.appendChild(button);
+
+    button.addEventListener('click', () => {
+        navigator.clipboard.writeText(block.innerText);
+        button.innerText = 'Copied!';
+        setTimeout(() => button.innerText = 'Copy', 2000);
+    });
+}
 
 async function sendQuest() {
     const jam = `${now.getHours().toString().padStart(2, '0')}.${now.getMinutes().toString().padStart(2, '0')}`;
@@ -42,7 +56,11 @@ async function sendQuest() {
         if(data.error) {
             lastMessage.querySelector(".bubble").innerHTML = `${data.error} <span class="time">${jam}</span>`;
         } else {
-            lastMessage.querySelector(".bubble").innerHTML = `${data.jawaban} <span class="time">${jam}</span>`;
+            const htmlJawaban = marked.parse(data.jawaban);
+            lastMessage.querySelector(".bubble").innerHTML = `<div class="markdown-content">${htmlJawaban}</div> <span class="time">${jam}</span>`;
+            lastMessage.querySelectorAll('pre code').forEach((block) => {
+            hljs.highlightElement(block);
+            btnCopy(block);
         }
     } catch(error) {
         lastMessage.querySelector(".bubble").innerHTML = `Error jaringan, silahkan coba lagi <span class="time">${jam}</span>`;
@@ -51,3 +69,4 @@ async function sendQuest() {
 } 
 
 btnSubmit.addEventListener('click', sendQuest);
+
