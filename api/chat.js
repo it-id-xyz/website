279a8e451df35c29c56aa9a0e9690066ce16974f
it-id-xyz <ayamsayrrr78@gmail.com>
2026-02-07 11:08:21 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,13 @@
 const axios = require('axios'); 
 
 export default async function handler(req, res) { 
+  if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'POST');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.status(200).end();
+        return;
+    }
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
