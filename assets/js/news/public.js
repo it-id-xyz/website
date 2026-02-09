f97eb9c50277729bb172132e13c160fe72e3aef5
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-02-09 22:26:10 +0700

    Update public.js

--- assets/js/news/public.js
+++ assets/js/news/public.js
@@ -7,20 +7,13 @@ if (containerArtikel) {
     containerArtikel.innerHTML = "";            
     snap.forEach((snap) => {
         const art = snap.data();
-        const docId = snap.id;
             containerArtikel.innerHTML += `
-                <div class="card-monitor">
+                <div class="news-card">
                     <img src="${art.foto}" style="width:100%; border-radius:8px;">
                     <p><strong>ID:</strong> ${art.id}</p>
                     <p>${art.judul}</p>
                     <p><small>Tgl: ${new Date(art.createdAt).toLocaleString('id-ID')}</small></p>
-                    <button class="delete-btn" data-id="${docId}" style="background:#ff4d4d; color:white; border:none; padding:5px; border-radius:4px; cursor:pointer;">
-                        <i class="fa-regular fa-trash-can"></i> Delete
-                    </button>
                 </div>`
             });
         });
     }
-
-
-})
