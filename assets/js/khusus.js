4067487ae7c8f0aacd05f01e6636e43ef1ee019c
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-02-09 10:33:51 +0700

    Update khusus.js

--- assets/js/khusus.js
+++ assets/js/khusus.js
@@ -24,30 +24,6 @@ async function refreshDashboard() {
                 reqBar.style.width = `${percent}%`;
             }
         }
-
-        // Tampilkan List Artikel Firestore
-        const containerArtikel = document.getElementById('total-articles');
-        if (containerArtikel) {
-            onSnapshot(collection(db,"article"), (snap) => {
-                containerArtikel.innerHTML = "";
-            
-                snap.forEach((snap) => {
-                    const art = snap.data();
-                    const docId = snap.id;
-                    containerArtikel.innerHTML += `
-                        <div class="card-monitor">
-                            <img src="${art.foto}" style="width:100%; border-radius:8px;">
-                            <p><strong>ID:</strong> ${art.id}</p>
-                            <p>${art.judul}</p>
-                            <p><small>Tgl: ${new Date(art.createdAt).toLocaleString('id-ID')}</small></p>
-                            <button class="delete-btn" data-id="${docId}" style="background:#ff4d4d; color:white; border:none; padding:5px; border-radius:4px; cursor:pointer;">
-                                <i class="fa-regular fa-trash-can"></i> Delete
-                            </button>
-                        </div>`
-                    });
-                });
-            }
-
         // Tampilkan Status Server & Firebase
         const ramRaw = data.server.ram;
         const ram = parseFloat(ramRaw);
@@ -269,8 +245,24 @@ requireAdmin().then(async (user) => {
     window.location.href = "login.html";
 });
 
-
-
-
-
+const containerArtikel = document.getElementById('total-articles');
+if (containerArtikel) {
+    onSnapshot(collection(db,"article"), (snap) => {
+    containerArtikel.innerHTML = "";            
+    snap.forEach((snap) => {
+        const art = snap.data();
+        const docId = snap.id;
+            containerArtikel.innerHTML += `
+                <div class="card-monitor">
+                    <img src="${art.foto}" style="width:100%; border-radius:8px;">
+                    <p><strong>ID:</strong> ${art.id}</p>
+                    <p>${art.judul}</p>
+                    <p><small>Tgl: ${new Date(art.createdAt).toLocaleString('id-ID')}</small></p>
+                    <button class="delete-btn" data-id="${docId}" style="background:#ff4d4d; color:white; border:none; padding:5px; border-radius:4px; cursor:pointer;">
+                        <i class="fa-regular fa-trash-can"></i> Delete
+                    </button>
+                </div>`
+            });
+        });
+    }
 
