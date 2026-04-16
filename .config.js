23af358e3985af3c6a45ebfb4b49daaf0b07c942
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-04-16 12:31:06 +0700

    Create .config.js

---
+++ .config.js
@@ -0,0 +1,7 @@
+export const CONFIG = {
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx5fPODBnuMb_WOfT-X8HzbW6MSt3Xp3uYqcxzUcsTDvCTMxBpfrcDNyvMbFEtDVf5D/exec",
+    TARGET_LAT: -6.4128827,
+    TARGET_LNG: 107.0842405,
+    TOLERANSI_METER: 50,
+    ANGKATAN_DEFAULT: "2024"
+};
