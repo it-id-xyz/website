9dd6f2d79b846559f8252b75077db1975aa66a00
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-04-27 21:04:17 +0700

    Update absen.js

--- assets/js/absen.js
+++ assets/js/absen.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase.js";
-import { collection, query, where, getDocs, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { collection, query, where, getDocs, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import * as faceapi from "https://cdn.jsdelivr.net/npm/@vladmandic/face-api/dist/face-api.esm.js";
 import { CONFIG } from "./config.js";
 
