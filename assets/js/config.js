709c6f9c2f0aa34351b736afaf97a376a1a680e2
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-05-10 11:57:16 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
     APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx5fPODBnuMb_WOfT-X8HzbW6MSt3Xp3uYqcxzUcsTDvCTMxBpfrcDNyvMbFEtDVf5D/exec",
-    TARGET_LAT: -6.4128827,
-    TARGET_LNG: 107.0842405,
+    TARGET_LAT: -6.4164736612194995, 
+    TARGET_LNG: 107.0629128450668,
     TOLERANSI_METER: 50,
     ANGKATAN_DEFAULT: "2024"
 };
