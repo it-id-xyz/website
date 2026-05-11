75c27c98db4c6f665003267d92a20524d1ef427e
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-05-11 08:14:30 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
     APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx8m03ehSa3c3-Q-XCcw1C2xiBD98145tXd8tf8iCCV4PBGhddh992kOzI-Gy4Bj7suFg/exec",
-    TARGET_LAT: -6.416480, 
-    TARGET_LNG: 107.062905,
+    TARGET_LAT: -6.412593, 
+    TARGET_LNG: 107.083222,
     TOLERANSI_METER: 50,
     ANGKATAN_DEFAULT: "2024",
     // 0=Minggu, 1=Senin, 2=Selasa, 3=Rabu, 4=Kamis, 5=Jumat, 6=Sabtu
