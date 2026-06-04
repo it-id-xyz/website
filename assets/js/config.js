de3dd2ebae714f90e7eb6dd42a333e1156bb6099
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-06-04 20:58:29 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,14 +1,16 @@
 export const CONFIG = {
     APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx8m03ehSa3c3-Q-XCcw1C2xiBD98145tXd8tf8iCCV4PBGhddh992kOzI-Gy4Bj7suFg/exec",
+    // ABSEN
     TARGET_LAT: -6.412858, 
     TARGET_LNG: 107.084255,
     TOLERANSI_METER: 50,
     ANGKATAN_DEFAULT: "2024",
-    // 0=Minggu, 1=Senin, 2=Selasa, 3=Rabu, 4=Kamis, 5=Jumat, 6=Sabtu
-    // Masukkan angka hari ke dalam kurung siku untuk mengizinkan absen di hari tersebut.
     HARI_ABSEN: [5], 
-    JAM_MULAI: 13,    // Jam mulai (13.00)
-    MENIT_MULAI: 30,  // Menit mulai (13.30)
-    JAM_SELESAI: 15,  // Jam selesai (15.00)
-    MENIT_SELESAI: 30 // Menit selesai (15.30)
+    JAM_MULAI: 13,  
+    MENIT_MULAI: 30, 
+    JAM_SELESAI: 15, 
+    MENIT_SELESAI: 30,
+    // CLOUDINARY
+    CLOUDINARY_NAME: "dviqru5xq",
+    CLOUDINARY_PRESET: "NewsCloud"
 };
