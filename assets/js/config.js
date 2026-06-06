61a254d36b6e15fc3fd3f744b9f705f017feb42a
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-06-07 01:38:05 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     APPS_SCRIPT_URL_ABSEN: "https://script.google.com/macros/s/AKfycbw5S9KZWWfPhTLN3roUNuG0GxGGm6xBl9Bb7nAyKs20zwfaVMnsvwGnUFG7ucdV-bMNLw/exec",
-    APPS_SCRIPT_URL_DB: "https://script.google.com/macros/s/AKfycbzDInOXD7r8gW-cetQ5llx5TQaZysIf7stHBIEp7IHmSnk2GzAwWhUqwO_ft480NPlJ/exec",
+    APPS_SCRIPT_URL_DB: "https://script.google.com/macros/s/AKfycbybILA09GyJ67V4RkqVGsZuo_wb9hJEL0ynTkUNH_-Cs5RyR-2xRLqErHb29ltTkfev/exec",
     // ABSEN
     TARGET_LAT: -6.412858, 
     TARGET_LNG: 107.084255,
